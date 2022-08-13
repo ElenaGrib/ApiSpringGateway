8cc641e6c8feb3748cbfdcee50ce2333ef27b7b4
ElenaGrib <elenagrib27@gmail.com>
2022-08-13 23:06:01 +0300

    Initial commit

---
+++ src/main/java/com/grib/api/gateway/apispringgateway/ApiSpringGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.grib.api.gateway.apispringgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiSpringGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSpringGatewayApplication.class, args);
+    }
+
+}
