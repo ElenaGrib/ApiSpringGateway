8f918787e3a8b827ac670399af50a1d16c364c8f
ElenaGrib <elenagrib27@gmail.com>
2022-08-20 11:05:51 +0300

    11. Validating JWT Access TokenURL

--- src/main/java/com/grib/api/gateway/apispringgateway/AuthorizationHeaderFilter.java
+++ src/main/java/com/grib/api/gateway/apispringgateway/AuthorizationHeaderFilter.java
@@ -56,11 +56,18 @@ public class AuthorizationHeaderFilter extends AbstractGatewayFilterFactory<Auth
     private boolean isJwtValid(String jwt) {
         boolean returnValue = true;
 
-        String subject = Jwts.parser()
-                .setSigningKey(environment.getProperty("token.secret"))
-                .parseClaimsJws(jwt)
-                .getBody()
-                .getSubject();
+        String subject = null;
+
+        try {
+            subject = Jwts.parser()
+                    .setSigningKey(environment.getProperty("token.secret"))
+                    .parseClaimsJws(jwt)
+                    .getBody()
+                    .getSubject();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            returnValue = false;
+        }
 
         if (subject == null || subject.isEmpty()) {
             returnValue = false;
